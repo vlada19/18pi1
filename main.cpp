b5c39b9d65a0302586f46d8637cb1c98f27601e6
vlada19 <48224272+vlada19@users.noreply.github.com>
2019-06-08 18:19:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,109 +1,64 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <windows.h>
+#include "hash.h"
 
-using namespace std;
-
-void readingFiles(string hashes, string* files, int number){
-    string fileData[number] = {""};
-    ofstream file;
-    hash<string> h;
-    file.open(hashes, ios::binary);
-    if(!file.is_open()){
-        cout<<"Couldn't open file!";
-        exit(1);
-    }
-    for(int i = 0; i < number; i++){
-        string line;
-        ifstream t;
-        t.open(files[i], ios::binary);
-        if(!t.is_open()){
-            cout<<"Couldn't open file!";
-            exit(1);
-        }
-        while(!t.eof()){
-            getline(t, line);
-            fileData[i] += line;
-        }
-        t.close();
-    }
-    for(int i = 0; i < number; i++){
-        file << files[i]<<endl << h(fileData[i])<<endl;
-    }
-    file.close();
-}
-
-void checkFiles(string hashes, string* files, int number){
-    string fileData[number] = {""};
-    for(int i = 0; i < number; i++){
-        string line;
-        ifstream t;
-        t.open(files[i], ios::binary);
-        if(!t.is_open()){
-            cout<<"Couldn't open file!";
-            exit(1);
-        }
-        while(!t.eof()){
-            getline(t, line);
-            fileData[i] += line;
-        }
-        t.close();
-    }
-
-    hash<string> h;
-    ifstream file;
-    file.open(hashes, ios::binary);
-    if(!file.is_open()){
-        cout<<"Couldn't open file!";
-        exit(1);
-    }
-    bool allRight = true;
-    for(int i = 0; i < number; i++){
-        string name;
-        size_t val;
-        while(!file.eof()){
-            file >> name;
-            file >> val;
-            if(name == files[i]){
-                if(val != h(fileData[i])){
-                    allRight = false;
-                    cout<<"Hashes do not match "<< files[i]<<endl;
-                }
-                file.clear();
-                file.seekg(0);
-                break;
-            }
-        }
-    }
-    if(allRight){
-        cout<<"All hashes match!"<<endl;
-    }
-    file.close();
-}
 
 int main(){
-    string hashFile;
+    setlocale(LC_ALL,"");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Hash one;   //файл класса
     char mode;
-    int sumFiles;
     cout<<"Reading or check (r/c)? ";
     cin>>mode;
-    cout<<"How many files will be? ";
-    cin>>sumFiles;
-    cout<<"Enter file names: ";
-    string files[sumFiles];
-    for(int i = 0; i < sumFiles; i++){
-        cin>>files[i];
+
+    WIN32_FIND_DATA FindFileData;
+    HANDLE hf;
+    wchar_t folderPath[75];
+    wchar_t folderPath_1[75];
+    cout<<"Enter path folder (example - c:\\Folder\\*): " << endl;
+    wcin >> folderPath;
+    int folderPathLen = wcslen(folderPath);
+    for(int i = 0; i < folderPathLen-1; i++) {
+        folderPath_1[i] = folderPath[i];
     }
+    hf = FindFirstFile(folderPath, &FindFileData);
+    wofstream fout("d:\\output.txt");
+    if (hf != INVALID_HANDLE_VALUE)
+    {
+        do
+        {
+            fout << folderPath_1 << FindFileData.cFileName << endl;
+            one.sumFiles++;
+
+        } while (FindNextFile(hf, &FindFileData) != 0);
+        FindClose(hf);
+    }
+    fout.close();
+    
+    ifstream file("d:\\output.txt");
+    one.sumFiles=one.sumFiles-2;
+    cout << "Number of files - " << one.sumFiles << endl;
+
+    string files[one.sumFiles];
+
+    string buffer;
+    cout << "Files: " << endl;
+    for (int i = 0; i < 2; i++)
+        getline(file, buffer);
+    for(int i = 0; i < one.sumFiles; i++){
+        getline(file, files[i]);
+        cout << files[i] << endl;
+    }
+    cout << endl;
+    file.close();
 
     if(mode == 'r'){
-        cout<<"Where save hashes? ";
-        cin>>hashFile;
-        readingFiles(hashFile, files, sumFiles);
+        cout<<"Where save hashes? (example - c:\\Folder\\hash.txt)" << endl;
+        cin>>one.hashFile;
+        one.readingFiles(files);
     }else if(mode == 'c'){
-        cout<<"Where read hashes? ";
-        cin>>hashFile;
-        checkFiles(hashFile, files, sumFiles);
+        cout<<"Where read hashes? (example - c:\\Folder\\hash.txt)" << endl;
+        cin>>one.hashFile;
+        one.checkFiles(files);
     }
 
     return 0;
